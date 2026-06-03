190597c4dcedd651412af411c4b4e248c9a83d48
wangdi_astro <14600647+wangdiastro@user.noreply.gitee.com>
2026-06-03 17:03:05 +0800

    fix(app): 为 SelectionPage 添加 ErrorBoundary 防止白屏

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,9 @@ function App() {
     return (
       <>
         <NavigationListener />
-        <SelectionPage />
+        <ErrorBoundary>
+          <SelectionPage />
+        </ErrorBoundary>
       </>
     );
   }
